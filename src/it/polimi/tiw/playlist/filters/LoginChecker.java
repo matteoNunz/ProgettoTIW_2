c4ecae521898f6e03445f0f4096d668fb2efd83d
matteoNunz <matteonunziante9999@gmail.com>
2021-06-09 18:01:32 +0200

    Same of before

---
+++ src/it/polimi/tiw/playlist/filters/LoginChecker.java
@@ -0,0 +1,45 @@
+package it.polimi.tiw.playlist.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginChecker implements Filter {
+
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+		System.out.print("Login checker filter executing ...\n");
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String loginpath = req.getServletContext().getContextPath() + "/login.html";
+
+		HttpSession s = req.getSession();
+		if (s.isNew() || s.getAttribute("user") == null) {
+			//res.setStatus(403);
+			//res.setHeader("Location", loginpath);
+			res.sendRedirect(loginpath);
+			System.out.print("Login checker FAILED...\n");
+			return;
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+}
