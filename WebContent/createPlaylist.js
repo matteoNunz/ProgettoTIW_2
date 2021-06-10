6028236242ad06bcc4561fa28db5ca3af3b1428a
matteoNunz <matteonunziante9999@gmail.com>
2021-06-10 21:54:41 +0200

    Added the reset of the form

--- WebContent/createPlaylist.js
+++ WebContent/createPlaylist.js
@@ -20,7 +20,7 @@
             makeCall("POST" , "CreatePlaylist" , form ,
                 function (x) {
                     console.log("CallBack function called");
-
+                    //TODO reset in form
                     if(x.readyState == XMLHttpRequest.DONE){
                         switch (x.status){
                             case 200:
@@ -39,6 +39,7 @@
                     }
                 }
             );
+            form.reset();
         }else{
             form.reportValidity();
         }
